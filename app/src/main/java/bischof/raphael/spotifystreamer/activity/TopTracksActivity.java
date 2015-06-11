31cba5d71341ec63df71a565805c8fa389bfd91e
Biche Biche <raphael.bischof@gmail.com>
2015-06-11 13:13:11 +0200

    fix: Up navigation keeps savedinstancestate now

--- app/src/main/java/bischof/raphael/spotifystreamer/activity/TopTracksActivity.java
+++ app/src/main/java/bischof/raphael/spotifystreamer/activity/TopTracksActivity.java
@@ -3,9 +3,12 @@
  */
 package bischof.raphael.spotifystreamer.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import bischof.raphael.spotifystreamer.R;
 
@@ -27,5 +30,18 @@ public class TopTracksActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_top_tracks, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent intent = NavUtils.getParentActivityIntent(this);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                NavUtils.navigateUpTo(this, intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
